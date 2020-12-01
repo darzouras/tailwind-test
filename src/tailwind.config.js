d2f7155d3d251a67ef189fb3d1af2ac361a1f476
Darlene Zouras <dzouras@gmail.com>
2020-12-01 16:14:31 -0600

    Uploaded project files

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        colors: {
+            white: "var(--color-white)",
+            primary: {
+                DEFAULT: "var(--color-primary)",
+                light: "var(--color-primary-light)",
+                dark: "var(--color-primary-dark)",
+            },
+            secondary: "var(--color-secondary)",
+        },
+        fontFamily: {
+            'sans': ['Viga', 'Arial', 'sans-serif'],
+            'serif': ['Noto Serif TC', 'serif'],
+        },
+    },
+}
